9c3f1e625cb00971df4d22582a133811677ebba9
Josias Montag <josias@montag.info>
2016-08-24 15:04:19 +0200

    enable use_cow_images on local storage nodes

--- deployment_scripts/images_type.pp
+++ deployment_scripts/images_type.pp
@@ -8,7 +8,9 @@ class fuel-plugin-compute-local-storage_images_type {
   service { $::nova::params::compute_service_name: }
 
   nova_config {
-    'libvirt/images_type':      value => 'default';
+    'libvirt/images_type': value => 'default';
+    'DEFAULT/use_cow_images': value => 'True';
+    'DEFAULT/disk_allocation_ratio': value => '1.25';
   }
 
   Nova_config<||> ~>
